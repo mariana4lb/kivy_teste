fd949fd288105d4b3e7790ad3bd5c3285490d749
mariana4lb <161875797+mariana4lb@users.noreply.github.com>
2024-05-06 15:25:00 -0300

    Update and rename sliderlabel.py to Kivy.Stack_Layout.py

---
+++ Kivy.Stack_Layout.py
@@ -0,0 +1,8 @@
+from kivy.uix.stacklayout import StackLayout
+
+layout_syack = StackLayout()
+
+from kivy.uix.button import Button
+
+botao1 = Button(text='Botão 1')
+botao2 = Button(text='Botão 2')
